c3b3727630f6fb698c21a59e07ddd05747855bce
Marina <e282241@edu.sbl.ch>
2023-11-13 11:49:01 +0100

    funktionierendes git

--- script.js
+++ script.js
@@ -1,10 +1,473 @@
 export function aufgabe01(args) {
   const input = args
   const result = []
+  for (let i = 0; i < input.length; i++)  {
+      const currentElement = input[i]
 
-  for (let i = 0; i < input.length; i++) {
-    const currentElement = input[i]
-    result.push(currentElement)
+    //Wenn das aktuelle Zeichen 'e' ist, hänge nichts an die Liste an  
+    if (currentElement === "e" ) {
+      result.push("")
+      //Wenn das aktuelle Zeichen 'E' ist, hänge nichts an die Liste an
+    } else if (currentElement === "E" ) {
+      result.push("")
+    }
+    else { //Das aktuelle Zeichen ist weder ein 'e' noch ein 'E', hängt also das aktuelle Zeichen an
+      result.push(currentElement)
+    }
   }
+  
   return result.join("")
+
 }
+
+
+  export function aufgabe02(args) {
+    const input = args
+    const result = []
+   for (let i = 0; i < input.length; i++) {
+     const currentElement = input[i]
+
+
+     // Wandle das aktuelle Element in einen Grossbuchstaben um
+        const capitalizedElement = currentElement.toUpperCase()
+     
+    //Hänge den Grossbuchstaben an das Resultat an...
+        result.push(capitalizedElement)
+     
+  }
+  return result.join("") 
+  }
+  
+
+  export function aufgabe03(args) {
+    const input = args
+    const result = []
+    let count = 0
+   
+    for (let i = 0; i < input.length; i++) {
+      const currentElement = input[i]
+      //Soll nur ein 'e' enthalten
+  if (currentElement === 'e'){
+    count = count + 1
+  }
+   
+  // Soll grosses 'E' zählen
+  else if (currentElement === 'E'){
+    count = count + 1
+  }
+    }
+    return count
+  
+  
+  return result.join("") 
+  }
+  
+  
+
+  export function aufgabe04(args) {
+    const input = args
+    const result = []
+   
+   
+    for (let i = 0; i < input.length; i++) {
+       const currentElement = input[i]
+       
+    }
+
+
+     
+   return result.join("") 
+  }
+  
+
+  export function aufgabe05(args) {
+    const input = args
+      const result = []
+    let capitalLetters = 0
+     
+     
+    for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+     
+    //prüft ob es einen Grossbuchstaben gibt
+    if (currentElement=== ".") {}
+    else if (currentElement===" "){}
+     
+    //wenn ein Element gleich ist, dann rechnet es plus 1.
+    else if (currentElement === currentElement.toUpperCase())
+    {capitalLetters++}
+    }
+     
+    if (capitalLetters > 0) {
+      return true
+    } else {
+       return false
+    }
+     
+    }
+    
+
+ 
+
+
+export function aufgabe06(args) {
+    const input = args
+    const result = []
+
+    for (let i = 0; i < input.length; i++) {
+     const currentElement = input[i]
+     
+
+     
+     
+  }
+  return result.join("") 
+  }
+  
+
+  export function aufgabe07(args) {
+    const input = args
+    const result = []
+
+    for (let i = 0; i < input.length; i++) {
+     const currentElement = input[i]
+     if(currentElement==="und") {}
+     else if(currentElement=== ""){}
+
+
+
+     
+     
+  }
+  
+  
+  
+  return result.join("") 
+}
+  
+ 
+  export function aufgabe08(args) {
+    const input = args
+    const result = []
+
+    for (let i = 0; i < input.length; i++) {
+     const currentElement = input[i]
+     if (currentElement === "e" ) {
+      result.push("3")
+     
+    
+    }
+    else {
+      result.push(currentElement)
+    }
+
+
+     
+     
+  }
+  return result.join("") 
+  }
+  
+
+  export function aufgabe09(args) {
+    const input = args
+    let len = 0
+    for (let i = 0; i < input.length; i++) {
+     len++
+    }
+    if (len === 6) {
+      return true
+    } else {
+      return false
+    }
+
+  } 
+     
+  
+  
+  
+
+  export function aufgabe10(args) {
+    const input = args
+    const result = []
+
+    for (let i = 0; i < input.lenght; i++) {
+     const currentElement = input[i]
+
+
+     
+     
+  }
+  return result.join("") 
+  }
+  
+
+  export function aufgabe11(args) {
+    const input = args
+    let ascii = 0
+
+    if (input.length > 1) {
+      return null
+    }
+
+   for (let i = 0; i < input.length; i++) {
+     const currentElement = input[i]
+     ascii = currentElement.charCodeAt(0)
+     return ascii  
+     
+  }
+  return null
+  }
+  
+  export function aufgabe12(args) {
+    const input = args
+
+    for (let i = 0; i < input.length; i++) {
+     const currentElement = input[i]
+     if (currentElement === 'e' ) {
+      return i
+     }
+     
+  }
+  return -1
+  }
+  
+
+  export function aufgabe13(args) {
+    const input = args
+    const result = []
+
+    for (let i = 0; i < input.lenght; i++) {
+     const currentElement = input[i]
+
+
+     
+     
+  }
+  return result.join("") 
+  }
+  
+
+  export function aufgabe14(args) {
+    const input = args
+    const result = []
+
+    for (let i = 0; i < input.lenght; i++) {
+     const currentElement = input[i]
+
+
+     
+     
+  }
+  return result.join("") 
+  }
+  
+
+  export function aufgabe15(args) {
+    const input = args
+    const result = []
+
+    for (let i = 0; i < input.lenght; i++) {
+     const currentElement = input[i]
+
+
+     
+     
+  }
+  return result.join("") 
+  }
+  
+
+  export function aufgabe16(args) {
+    const input = args
+    const result = []
+
+    for (let i = 0; i < input.lenght; i++) {
+     const currentElement = input[i]
+
+
+     
+     
+  }
+  return result.join("") 
+  }
+  
+  export function aufgabe17(args) {
+    const input = args
+    const result = []
+
+    for (let i = 0; i < input.lenght; i++) {
+     const currentElement = input[i]
+
+
+     
+     
+  }
+  return result.join("") 
+  }
+  
+  export function aufgabe18(args) {
+    const input = args
+    const result = []
+
+    for (let i = 0; i < input.lenght; i++) {
+     const currentElement = input[i]
+
+
+     
+     
+  }
+  return result.join("") 
+  }
+  
+
+  export function aufgabe19(args) {
+    const input = args
+    const result = []
+
+    for (let i = 0; i < input.lenght; i++) {
+     const currentElement = input[i]
+
+
+     
+     
+  }
+  return result.join("") 
+  }
+  
+
+  export function aufgabe20(args) {
+    const input = args
+    const result = []
+
+    for (let i = 0; i < input.lenght; i++) {
+     const currentElement = input[i]
+
+
+     
+     
+  }
+  return result.join("") 
+  }
+  
+
+  export function aufgabe21(args) {
+    const input = args
+    const result = []
+
+    for (let i = 0; i < input.lenght; i++) {
+     const currentElement = input[i]
+
+
+     
+     
+  }
+  return result.join("") 
+  }
+  
+
+  export function aufgabe22(args) {
+    const input = args
+    const result = []
+
+    for (let i = 0; i < input.lenght; i++) {
+     const currentElement = input[i]
+
+
+     
+     
+  }
+  return result.join("") 
+  }
+  
+
+  export function aufgabe23(args) {
+    const input = args
+    const result = []
+
+    for (let i = 0; i < input.lenght; i++) {
+     const currentElement = input[i]
+
+
+     
+     
+  }
+  return result.join("") 
+  }
+  
+  export function aufgabe24(args) {
+    const input = args
+    const result = []
+
+    for (let i = 0; i < input.lenght; i++) {
+     const currentElement = input[i]
+
+
+     
+     
+  }
+  return result.join("") 
+  }
+  
+
+  export function aufgabe25(args) {
+    const input = args
+    const result = []
+
+    for (let i = 0; i < input.lenght; i++) {
+     const currentElement = input[i]
+
+
+     
+     
+  }
+  return result.join("") 
+  }
+  
+
+  export function aufgabe26(args) {
+    const input = args
+    const result = []
+
+    for (let i = 0; i < input.lenght; i++) {
+     const currentElement = input[i]
+
+
+     
+     
+  }
+  return result.join("") 
+  }
+  
+
+  export function aufgabe27(args) {
+    const input = args
+    const result = []
+
+    for (let i = 0; i < input.lenght; i++) {
+     const currentElement = input[i]
+
+
+     
+     
+  }
+  return result.join("") 
+  }
+  
+  export function aufgabe28(args) {
+    const input = args
+    const result = []
+
+    for (let i = 0; i < input.lenght; i++) {
+     const currentElement = input[i]
+
+
+     
+     
+  }
+  return result.join("") 
+  }
+  
+
+ 
+  
